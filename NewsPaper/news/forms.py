31d58698d471efc6ef56d09972ceb00038644b06
Vlad2692 <Lad2692@yandex.ru>
2023-07-01 21:30:38 +0300

    check

---
+++ NewsPaper/news/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Post
+class PostForm(forms.ModelForm):
+    # description = forms.CharField(min_length=20)
+    class Meta:
+       model = Post
+       fields = [
+           'author',
+           'type',
+           'header',
+           'category',
+           'content',
+       ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        content = cleaned_data.get("content")
+        header = cleaned_data.get("header")
+
+        if header == content:
+            raise ValidationError(
+                "Описание не может быть индентично названию."
+            )
+
+        return cleaned_data
---
+++ NewsPaper/news/views.py
@@ -0,0 +1,84 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, \
+    DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Author
+from .filters import PostFilter
+from .forms import PostForm
+from django.shortcuts import render
+
+
+class PostList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-time_in'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'news.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+    paginate_by = 10
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'new.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
+
+class PostSearchList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-time_in'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'search.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+from django.shortcuts import render
+class PostCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # form_valid = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        if 'news' in self.request.path:
+            type_ = 'NW'
+        elif 'article' in self.request.path:
+            type_ = 'ST'
+        self.object.type = type_
+        # post = form.save(commit=False)
+        # post.type = 13
+        return super().form_valid(form)
+# Create your views here.
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+# Create your views here.
